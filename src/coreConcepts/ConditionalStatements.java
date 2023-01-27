058d65f9ceae0a06514eca37af232f0c0461fc11
NagKothapalli <nag022@gmail.com>
2023-01-27 21:02:54 +0530

    Jan27th code pushed

--- src/coreConcepts/ConditionalStatements.java
+++ src/coreConcepts/ConditionalStatements.java
@@ -7,6 +7,24 @@ public class ConditionalStatements
 	int a = 22; //fixed value
 	int b = 25; //Assignment operator
 	
+	
+	//WAP to read two integers as input and perform sum of two numbers and return the sum as output
+	public void addition_test()
+	{
+		int output = addition(20,40);
+		addition(12,16,18);
+	}
+	
+	
+	public void addition(int a,int b,int c)
+	{
+		int sum = a+b+c; //
+	}
+	public int addition(int a,int b)
+	{
+		int sum = a+b; // 
+		return sum;
+	}
 	//if(expression -> boolean : true | false){ true TASK }
 	@Test
 	public void compareNumbers_if()
@@ -60,4 +78,67 @@ public class ConditionalStatements
 		}
 	}
     // code debugging : Process of executing the program line by line by the user .
+	//WAP to read two integers as input and perform sum of two numbers if equal , a>b do subtraction , a<b do multiplication
+	@Test
+	public void demonstrate_elseif_test()
+	{
+		demonstrate_elseif(22,22);
+		demonstrate_elseif(42,22);
+		demonstrate_elseif(22,62);
+	}
+	public void demonstrate_elseif(int a,int b)
+	{
+		System.out.println("First Number :"+ a);
+		System.out.println("Second Number :"+ b);
+		if(a==b) 
+		{ //true
+			//TASK
+			int sum = a+b;
+			System.out.println("Sum of two numbers :" + sum);
+		}
+		else if(a>b)
+		{ //false
+			int diff = a-b;
+			System.out.println("Difference of two numbers :" + diff);
+		}
+		else if(a<b)
+		{
+			int prod = a*b;
+			System.out.println("Product of two numbers:"+prod);
+		}
+	}
+	
+	public void thisIsNonStatic()
+	{
+		System.out.println("This is my Non Static Function");
+	}
+	public static void thisIsStatic()
+	{
+		System.out.println("This is my Static Function");
+	}
+	//switch case , Nested if
+	
 }
+
+class Consumer
+{
+	public void demoStaticKeyWord()
+	{
+		ConditionalStatements.thisIsStatic();//only static members can be accessed thru class name
+		ConditionalStatements myObject = new ConditionalStatements();
+		myObject.compareNumbers_if();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
