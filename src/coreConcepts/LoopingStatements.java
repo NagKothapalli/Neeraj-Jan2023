7e2a4417ccd32cfd7e90666aafcba25ae53f38b6
NagKothapalli <nag022@gmail.com>
2023-02-02 19:59:28 +0530

    Feb2nd code pushed

--- src/coreConcepts/LoopingStatements.java
+++ src/coreConcepts/LoopingStatements.java
@@ -29,5 +29,104 @@ public class LoopingStatements
 			System.out.println("Do a Round :" + counter);
 		}
 	}
+	
+	//WAP to appy color to steps in building
+	@Test
+	public void applyColorToSteps_part1() //Definite Loops
+	{
+		for(int step=1;step<=50;step=step+1) //step = 1  1<50 - > true 49<50
+		{ //true
+			System.out.println("Apply Color to Step :"+ step);
+		}
+	}
+	
+	@Test
+	public void applyColorToSteps_part2() //Definite Loops
+	{
+		for(int step=51;step<=100;step=step+1) //step = 1  1<50 - > true 49<50
+		{ //true
+			System.out.println("Apply Color to Step :"+ step);
+		}
+	}
+	//WAP to generate Odd Numbers in the range of 1-50
+	@Test
+	public void applyGreenColorToEven()
+	{
+		for(int s=2;s<=50;s=s+2)
+		{
+			System.out.println("Apply Green Color to Step :" + s);
+		}
+	}
+	
+	//WAP to generate Even Numbers in the range of 1-50
+	@Test
+	public void applyRedColorToOdd()
+	{
+		for(int s=1;s<=50;s=s+2)
+		{
+			System.out.println("Apply Red Color to Step :" + s);
+		}
+	}
+	@Test
+	public void findStudent() //22 66
+	{
+		boolean flag = false;
+		for(int i=1;i<=60;i++)
+		{
+			//System.out.println("Students Searching :" + i);
+			if(i == 36)
+			{
+				//System.out.println("Found the Student");
+				flag = true;
+				break;
+			}			
+		}
+		if(flag == true)
+		{
+			System.out.println("Found the Student");
+		}
+		else
+		{
+			System.out.println("Did not Found the Student");
+		}
+	}
+	@Test
+	public void findStudent_test()
+	{
+		boolean result = findStudent(84);
+		if(result == true)
+		{
+			System.out.println("Found the Student");
+		}
+		else
+		{
+			System.out.println("Make a call to his parents and inform ");
+		}
+	}
+	
+	public boolean findStudent(int stdNum) //22 66  //Indefinite
+	{
+		boolean flag = false;
+		for(int i=1;i<=60;i++)
+		{
+			//System.out.println("Students Searching :" + i);
+			if(i == stdNum)
+			{
+				//System.out.println("Found the Student");
+				flag = true;
+				break;
+			}			
+		}
+		return flag;
+	}
+	
+	//Nested Loop
+	
+	
+	
+	
+	
+	
+	
 
 }
