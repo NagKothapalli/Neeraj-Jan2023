79ccc4acc4d198729e565a73c23fce032546bb35
NagKothapalli <nag022@gmail.com>
2023-03-03 21:13:12 +0530

    March3rd code pushed

--- src/seleniumPractice/GmailAutomation.java
+++ src/seleniumPractice/GmailAutomation.java
@@ -1,8 +1,12 @@
 package seleniumPractice;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import junit.framework.Assert;
@@ -15,15 +19,20 @@ public class GmailAutomation
 	//junit.framework.ComparisonFailure: expected:<[A]mail> but was:<[G]mail>
 	
 	//WebDriver : get(), getTitle , getCurrentUrl , getWindowHandle , findElement ,
+	WebDriver driver;
 	String expectedTitle = "Gmail";
-	@Test
+	public GmailAutomation() //default constructor
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
+		driver = new ChromeDriver();//it will open an empty chrome browser  //1234
+	}
+	//Execution Flow : Class Variables -> Constructor -> Before -> Test	
+	@Before
 	public void launchApplication()
 	{
 		System.out.println("Launch Application");
-		//open an empty browser  then call your app URL in that browser
-		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();//it will open an empty chrome browser
-		driver.get("https://gmail.com");
+		//open an empty browser  then call your app URL in that browser		
+		driver.get("https://gmail.com"); //1234
 		String actualtitle = driver.getTitle();
 		System.out.println("title of the app :" + actualtitle);
 		String url = driver.getCurrentUrl();
@@ -42,12 +51,46 @@ public class GmailAutomation
 		}
 
 		Assert.assertEquals(expectedTitle, actualtitle);
-		driver.findElement(By.name("identifier")).click();
-		driver.findElement(By.name("identifier")).sendKeys("abcdfsdffg");
-		driver.findElement(By.name("identifier")).clear();
-		driver.findElement(By.name("identifier")).sendKeys("nag022");
+		
 	}
 	
 	//DOM : Data Object Model - It will contain all the properties of webelements in side the web page
-
+   //Execution Flow : Class Variables -> Constructor -> Before -> Test
+	@Test
+	public void loginToApplication()
+	{	
+		//driver.findElement(By.name("identifier")).click(); //1234
+		//driver.findElement(By.name("identifier")).sendKeys("abcdfsdffg");
+		//driver.findElement(By.name("identifier")).clear();
+		//driver.findElement(By.name("identifier")).sendKeys("9959775757");
+		WebElement email = driver.findElement(By.name("identifier"));
+		email.click();
+		email.sendKeys("dfdsfsds");
+		email.clear();
+		email.sendKeys("9959775757");
+		List<WebElement> elements = driver.findElements(By.className("VfPpkd-vQzf8d"));
+		WebElement element = elements.get(1);
+		element.click();
+	}
+	@Test
+	public void createAccount()
+	{
+		//List<WebElement> elements = driver.findElements(By.className("VfPpkd-vQzf8d"));
+		//WebElement element = elements.get(2);
+		//element.click();
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(2).click();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
